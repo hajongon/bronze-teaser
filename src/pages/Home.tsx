2c17ef9d6ef89a6edd88f3e742b51ae9ba622b9a
hajongon <hajongon@gmail.com>
2024-03-05 15:22:11 +0900

    Feat: 위치 개별 지정 (#2)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -29,12 +29,42 @@ function Home() {
     //   }
     // }
 
-    for (let y = 0; y < words.length; y++) {
-      // 예: 모든 단어를 같은 X좌표에 배치하고 Y좌표만 증가시킵니다.
-      // 요소 하나하나마다 원하는 position을 push해주면 됨 -> 이러면 끝
-      positions.push({ x: 10, y: y * 3 })
-    }
-    return positions.sort(() => Math.random() - 0.5) // 위치를 무작위로 섞음
+    // for (let y = 0; y < words.length; y++) {
+    //   // 예: 모든 단어를 같은 X좌표에 배치하고 Y좌표만 증가시킵니다.
+    //   // 요소 하나하나마다 원하는 position을 push해주면 됨 -> 이러면 끝
+    //   positions.push({ x: 10, y: y })
+    // }
+    positions.push({ x: 10, y: 0 })
+    positions.push({ x: 18, y: 1 })
+    positions.push({ x: 18, y: 1 })
+    positions.push({ x: 18, y: 1 })
+    positions.push({ x: 18, y: 1 })
+    positions.push({ x: 18, y: 1 })
+    positions.push({ x: 18, y: 1 })
+    positions.push({ x: 18, y: 1 })
+    positions.push({ x: 18, y: 1 })
+    positions.push({ x: 18, y: 1 })
+    positions.push({ x: 18, y: 1 })
+    positions.push({ x: 18, y: 1 })
+    positions.push({ x: 18, y: 1 })
+    positions.push({ x: 18, y: 1 })
+    positions.push({ x: 18, y: 1 })
+    positions.push({ x: 18, y: 1 })
+    positions.push({ x: 18, y: 1 })
+    positions.push({ x: 18, y: 1 })
+    positions.push({ x: 18, y: 1 })
+    positions.push({ x: 18, y: 1 })
+    positions.push({ x: 18, y: 1 })
+    positions.push({ x: 18, y: 1 })
+    positions.push({ x: 18, y: 1 })
+    positions.push({ x: 18, y: 1 })
+    positions.push({ x: 18, y: 1 })
+    positions.push({ x: 18, y: 1 })
+    positions.push({ x: 18, y: 1 })
+    positions.push({ x: 18, y: 1 })
+    positions.push({ x: 18, y: 1 })
+    positions.push({ x: 18, y: 1 })
+    return positions // 위치를 무작위로 섞음
   }
 
   const [gridPositions] = useState(createGridPositions())
@@ -108,7 +138,7 @@ function Home() {
             className={styles.word}
             style={{
               left: `${wordObject.position.x * 4}%`,
-              top: `calc(${wordObject.position.y * (100 / 20)}% - 103rem)`,
+              top: `calc(${wordObject.position.y * (100 / 30)}% - 6rem)`,
             }}
           >
             {wordObject.text}
